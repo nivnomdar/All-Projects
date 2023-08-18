cedb259aeb02d0fe93e680e5e7276fae5c8634da
niv nomdar <nivnomdar1@gmail.com>
2023-08-18 13:29:09 +0300

    Friends-List
    Friends-List with photos

---
+++ Friends-List/friends.js
@@ -0,0 +1,33 @@
+var friendList = [];
+
+const getData = () => {
+
+  var fName = document.getElementById("fName").value;
+  var fTel = document.getElementById("fTel").value;
+  var fPhoto = document.getElementById("fImg").value;
+  console.log(fName, fTel, fPhoto);
+
+  var newFriend = new Object();
+  newFriend.name = fName;
+  newFriend.tel = fTel;
+  newFriend.photo = fPhoto;
+  friendList.push(newFriend);
+
+  console.log(friendList);
+  document.getElementById("friendForm").reset();
+  makeTable();
+};
+
+const makeTable = () => {
+  var result = "";
+  for (var index = 0; index < friendList.length; index++) {
+    result += `
+            <tr>
+                <td>${friendList[index].name}</td>
+                <td>${friendList[index].tel}</td>
+                <td><img src="${friendList[index].photo}" width="100"/></td>
+            </tr>
+        `;
+  }
+  document.getElementById("friendTable").innerHTML = result;
+};
